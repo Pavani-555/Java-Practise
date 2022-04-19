bf8218f3e1a071c1ad394fab1a86cac23f2a7b20
Pavani-555 <103115686+Pavani-555@users.noreply.github.com>
2022-04-19 16:40:16 +0530

    Add files via upload

---
+++ TableWhile.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class TableWhile
+  {
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter number:");
+    int num=sc.nextInt();
+    System.out.println("Enter range:");
+    int range=sc.nextInt();
+    int i=1;
+    while(i<=range)
+      int i=1;
+    while(i<=range)
+      {
+        System.out.println(num+ "*"+i+"="+num*i);
+        i++;
+        }
+}
+}
+  
+
+  
---
+++ Vowel_Consonant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Vowel_Consonant
+{
+public static void main(String args[])
+{
+System.out.println("Enter a character :");
+Scanner vc=new Scanner(System.in);
+  char ch=vc.next(),charAt(0);
+  if(ch=='a'|| ch=='e'||ch=='i' ||ch=='o'||ch=='u')
+  {
+    System.out.println("Given Character is an conosonant");
+    }
+  else
+  {
+    System.out.println("Given character is a consonant");
+  }
+    
+  }
+  }
+
+
